d2a03c0b05dfd4ec6c4fc6a188513b4800ee08bb
winfreykong <110858679+winfreykong@users.noreply.github.com>
2024-01-29 13:30:41 -0800

    Add files via upload

---
+++ dist_util.py
@@ -0,0 +1,55 @@
+"""
+Helpers for distributed training.
+"""
+
+import io
+import os
+import socket
+
+import blobfile as bf
+
+import torch as th
+import torch.distributed as dist
+
+# Change this to reflect your cluster layout.
+
+def clear_cache():
+    if th.cuda.is_available():
+        th.cuda.empty_cache()
+
+def dev():
+    """
+    Get the device to use for torch.distributed.
+    """
+    if th.cuda.is_available():
+        return th.device("cuda")
+    return th.device("cpu")
+
+
+def load_state_dict(path, **kwargs):
+    """
+    Load a PyTorch file.
+    """
+    # if int(os.environ['LOCAL_RANK']) == 0:
+    with bf.BlobFile(path, "rb") as f:
+        data = f.read()
+    return th.load(io.BytesIO(data), **kwargs)
+
+
+def sync_params(params):
+    """
+    Synchronize a sequence of Tensors across ranks from rank 0.
+    """
+    for p in params:
+        with th.no_grad():
+            dist.broadcast(p, 0)
+
+
+def _find_free_port():
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.bind(("", 0))
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        return s.getsockname()[1]
+    finally:
+        s.close()
