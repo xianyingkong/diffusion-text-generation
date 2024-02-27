7eeb2155621d8cfb93dfe9cb96b8c331aeeff35f
Alexander Jensen <adjensen@ucsd.edu>
2024-02-27 11:46:51 -0800

    Added working tokenizer function

--- create_tokenizer.py
+++ create_tokenizer.py
@@ -3,40 +3,45 @@ import numpy as np
 import os
 from transformers import AutoTokenizer, PreTrainedTokenizerFast
 
-
-tokenizer_save_path = 'shakespeare-tokenizer-bert'
-data_path = os.path.join('data', 'shakespeare.csv')
-vocab_size = 32000 # Change if desired
-
-def create_tokenizer(corpus_path: str, vocab_size: int, preprocessing=None):
+def create_tokenizer(corpus_path: str, vocab_size: int = 32000, preprocessing=None, save_folder=False):
     """ Creates a tokenizer from given corpus_path and vocab_size.
     Arguments:
         corpus_path (str) - path leading to a file containing all words to be included in vocabulary.
         vocab_size (int) - size of vocabulary for tokenizer
-        preprocessing (func) - a function to preprocess/clean data contained in corpus_path.
+        preprocessing (func) - a function to preprocess/clean data contained in corpus_path.read()
+        save_folder (None/str) - if str, saves to specified folder path.
     """
-    with open(corpus_path
-# Open file with iteration
-with open(data_path, 'r') as corpus:
-    new_tokenizer = bert_tokenizer.train_new_from_iterator(corpus.readlines(), vocab_size)
+ 
+    # Create and train tokenizer
+    bert_tokenizer = AutoTokenizer.from_pretrained('bert-base-uncased') # Bert will be the base tokenizer
+    with open(corpus_path, 'r') as corpus:
+        corpus_list = corpus.readlines()
+        example = corpus_list[len(corpus_list) // 4] # Pick a line for example
+        
+        # Preprocess if needed
+        if preprocessing is not None:
+            corpus_list = preprocessing(corpus.read())
 
+        new_tokenizer = bert_tokenizer.train_new_from_iterator(corpus_list, vocab_size)
 
+    # Display output for user
+    print('New tokenizer created')
+    # Sanity check for tokenizer
+    
+    print(f'Tokenizer contains vocab size {new_tokenizer.vocab_size}')
+    test_tokenize = new_tokenizer.tokenize(example)
+    test_encode = new_tokenizer.encode(example)
+    print(f'Example: {example}')
+    print(f'Example Tokenized line (length {len(test_tokenize)}): {test_tokenize}')
+    print(f'Example Tokenized Index (length {len(test_encode)}): {test_encode}')
 
-# Creates new tokenizer with our vocabulary set
-new_tokenizer = bert_tokenizer.train_new_from_iterator(lines_iter, 32000)
+    # Save tokenizer
+    if isinstance(save_folder, str):
+        new_tokenizer.save_pretrained(save_folder) # Used for saving the tokenizer
+        print(f'Tokenizer saved at {save_folder}')
+        print(f'Tokenizer can be loaded by BertTokenizerFast(vocab_file="{save_folder}")')
 
-# Just for sanity check
-print(f'Tokenizer contains vocab size {new_tokenizer.vocab_size}')
-test_tokenize = new_tokenizer.tokenize(data.PlayerLine.iloc[0])
-test_encode = new_tokenizer.encode(data.PlayerLine.iloc[0])
-print(f'Example Tokenized line (length {len(test_tokenize)}): {test_tokenize}')
-print(f'Example Tokenized Index (length {len(test_encode)}): {test_encode}')
-
-#with open('shakespeare_lines.raw', 'r') as shakespeare_lines:
-#    new_tokenizer = bert_tokenizer.train_new_from_iterator(shakespeare_lines, 32000)
-
-# Save
-new_tokenizer.save_pretrained("shakespeare-tokenizer-bert")
-
-vocab_path = os.path.join(tokenizer_save_path, 'vocab.txt')
-print(f'Tokenizer can be loaded by BertTokenizerFast(vocab_file="{vocab_path}")')
+    
+    return new_tokenizer
+        
+    
